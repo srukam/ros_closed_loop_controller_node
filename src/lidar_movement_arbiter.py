e7ea742de7b688b659011b55f2a142ed582a56b3
srukam <srujanaren@gmail.com>
2026-01-22 20:37:25 -0800

    Behavior-Level Autonomous Navigation with Safety Arbitration

--- src/lidar_movement_arbiter.py
+++ src/lidar_movement_arbiter.py
@@ -5,11 +5,20 @@ import tf
 import math
 import time
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 safety_velocity = None
 desired_velocity = None
-current_speed = None
+current_state = "IDLE"
+
+
+def stop_robot0():
+    stop_vel = Twist()
+    stop_vel.linear.x = 0.0
+    stop_vel.angular.z = 0.0
+    actual_vel_pub.publish(stop_vel)
+    rospy.sleep(0.5)
 
 
 def safety_velocity_cb(safevel):
@@ -21,23 +30,34 @@ def desired_velocity_cb(desvel):
     desired_velocity = desvel
 
 def publishing_cb(event):
+    global current_state
     actual_velocity = Twist()
+    
     if safety_velocity is None or desired_velocity is None:
+        current_state = "IDLE"
+        state_pub.publish(current_state)
         return
     if safety_velocity.linear.x < desired_velocity.linear.x:
         actual_velocity.linear.x = safety_velocity.linear.x
+        if actual_velocity.linear.x == 0 :
+            current_state = "STOP"
+        else:
+            current_state = "SLOW"
     else:
         actual_velocity.linear.x = desired_velocity.linear.x
-        current_speed = desired_velocity.linear.x
-    
+
+        current_state = "MOVE"
     actual_velocity.angular.z = desired_velocity.angular.z
     actual_vel_pub.publish(actual_velocity)
+    state_pub.publish(current_state)
         
 
 
 if __name__ == "__main__":
     rospy.init_node("Lidar_movement_arbiter")
     actual_vel_pub = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
+    rospy.on_shutdown(stop_robot0)
+    state_pub = rospy.Publisher("/robot_state",String,queue_size=10)
     safety_velocity_sub = rospy.Subscriber("/safety_cmd_vel",Twist,safety_velocity_cb)
     desired_velocity_sub = rospy.Subscriber("/desired_cmd_vel",Twist,desired_velocity_cb)
     publishing_timer = rospy.Timer(rospy.Duration(0.1),publishing_cb)
--- src/lidar_obstacle_mgmt.py
+++ src/lidar_obstacle_mgmt.py
@@ -15,11 +15,12 @@ start_index = None
 end_index = None
 min_front_distance = None
 ttc = None
-ttc_stop = 1.0
-ttc_slow = 2.0
+ttc_stop = 0.8
+ttc_slow = 1.5
 effective_stop = 0.0
 latest_scan = None
 current_speed = 0.0
+current_state = None
 
 #Parameter Initializations
 stop_distance = rospy.get_param("~stop_distance",0.6)
@@ -28,6 +29,11 @@ moving_speed = rospy.get_param("~moving_speed",0.6)
 front_min_angle = math.radians(rospy.get_param("~front_min_angle",-30))
 front_max_angle = math.radians(rospy.get_param("~front_max_angle",30))
 
+
+def statesub_cb(stat):
+    global current_state
+    current_state = stat.data
+    
 
 def cmdvel_cb(mds):
     global current_speed
@@ -73,53 +79,65 @@ def publish_front_arc_marker():
 
 def safety_timer_cb(event):
     global start_index,end_index, min_front_distance, ttc, effective_stop
-    
-    if latest_scan is None:
-        return
-    if start_index is None or end_index is None:
-        # front_min_angle = rospy.get_param("~front_min_angle",front_min_angle)
-        # front_max_angle = rospy.get_param("~front_max_angle",front_max_angle)
-        start_index = max(int((front_min_angle - latest_scan.angle_min) / latest_scan.angle_increment),0)
-        end_index = min(int((front_max_angle - latest_scan.angle_min) / latest_scan.angle_increment), len(latest_scan.ranges)-1)
-    front_ranges = latest_scan.ranges[start_index : (end_index+1)]
-    front_ranges = [x for x in front_ranges if not math.isnan(x) and not math.isinf(x)]
-    center_index = (start_index + end_index) // 2
-    if not front_ranges:
-        return
-    min_front_distance = latest_scan.ranges[center_index]
-    rospy.loginfo(f"Minimum front distance: {min_front_distance:.02f} ")
-    publish_front_arc_marker()
-    # stop_distance = rospy.get_param("~stop_distance",stop_distance)
-    # safe_distance = rospy.get_param("~safe_distance",safe_distance)
-    # moving_speed = rospy.get_param("~moving_speed",moving_speed)
-    effective_stop = stop_distance + 0.5 * moving_speed
-    if current_speed >0.05:
-        ttc = min_front_distance / current_speed
-    else:
-        ttc = float("inf")
     safety_velocity = Twist()
-    if min_front_distance<=effective_stop or (ttc <= ttc_stop):
+    if current_speed is None:
+        return
+    if current_state in ["IDLE","STOP"]:
         safety_velocity.linear.x = 0.0
-        rospy.loginfo("Hard Stop - Wall ahead")
-        
-    elif min_front_distance>=safe_distance and (ttc >= ttc_slow):
-        safety_velocity.linear.x = moving_speed
-        
+        safety_velocity.angular.z = 0.0
+        safety_pub.publish(safety_velocity)
+        return
     else:
-        dist_scale = (min_front_distance - stop_distance) / (safe_distance - stop_distance)
-        dist_scale = max(0.0, min(1.0, dist_scale))
-        ttc_scale = (ttc - ttc_stop) / (ttc_slow - ttc_stop)
-        ttc_scale = max(0.0, min(1.0, ttc_scale))
-        scale = min(dist_scale,ttc_scale)
-        safety_velocity.linear.x = scale * moving_speed
+        if latest_scan is None:
+            return
+        if start_index is None or end_index is None:
+            # front_min_angle = rospy.get_param("~front_min_angle",front_min_angle)
+            # front_max_angle = rospy.get_param("~front_max_angle",front_max_angle)
+            start_index = max(int((front_min_angle - latest_scan.angle_min) / latest_scan.angle_increment),0)
+            end_index = min(int((front_max_angle - latest_scan.angle_min) / latest_scan.angle_increment), len(latest_scan.ranges)-1)
+        front_ranges = latest_scan.ranges[start_index : (end_index+1)]
+        front_ranges = [x for x in front_ranges if not math.isnan(x) and not math.isinf(x)]
+        # center_index = (start_index + end_index) // 2
+        if not front_ranges:
+            return
+        min_front_distance = min(front_ranges)
+        rospy.loginfo(f"Minimum front distance: {min_front_distance:.02f} ")
+        publish_front_arc_marker()
+        # stop_distance = rospy.get_param("~stop_distance",stop_distance)
+        # safe_distance = rospy.get_param("~safe_distance",safe_distance)
+        # moving_speed = rospy.get_param("~moving_speed",moving_speed)
+        effective_stop = stop_distance + 0.5 * moving_speed
+        if current_speed >0.05:
+            ttc = min_front_distance / current_speed
+        else:
+            ttc = float("inf")
+        
+        if min_front_distance<=effective_stop or (ttc <= ttc_stop):
+            safety_velocity.linear.x = 0.0
+            
+            rospy.loginfo("Hard Stop - Wall ahead")
+            
+        elif min_front_distance>=safe_distance and (ttc >= ttc_slow):
+            safety_velocity.linear.x = moving_speed
+            
+            
+        else:
+            dist_scale = (min_front_distance - stop_distance) / (safe_distance - stop_distance)
+            dist_scale = max(0.0, min(1.0, dist_scale))
+            ttc_scale = (ttc - ttc_stop) / (ttc_slow - ttc_stop)
+            ttc_scale = max(0.0, min(1.0, ttc_scale))
+            scale = min(dist_scale,ttc_scale)
+            safety_velocity.linear.x = scale * moving_speed
+        
     safety_velocity.angular.z = 0.0
-    
+    rospy.loginfo(f"Current state is {current_state}")
     safety_pub.publish(safety_velocity)
 
 
 if __name__ == "__main__":
     rospy.init_node("Laser_safety_node")
     laser_scanner = rospy.Subscriber("/scan",LaserScan,laser_cb)
+    state_subs = rospy.Subscriber("/robot_state",String,statesub_cb)
     cmd_vel_sub = rospy.Subscriber("/cmd_vel",Twist,cmdvel_cb)
     safety_pub = rospy.Publisher("/safety_cmd_vel",Twist,queue_size=10)
     marker_pub = rospy.Publisher("/front_arc_marker",Marker,queue_size=1)
